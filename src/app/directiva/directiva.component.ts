062a489f586306b624dee554126731ac897d7b65
unknown <coke.jgomez@gmail.com>
2023-01-07 15:09:30 -0300

    Se agrega a la directiva ngfor con un listado

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directiva',
-  templateUrl: './directiva.component.html',
-  styleUrls: ['./directiva.component.css']
+  templateUrl: './directiva.component.html'
 })
 export class DirectivaComponent {
+
+listaCurso: string[] = ['TypeScript','JavaScript','Java SE','C#','PHP','Python']
+
+constructor() {}
 
 }
