e51d493cf69c8298f50059858ba628df3d0cb74c
unknown <coke.jgomez@gmail.com>
2023-01-08 19:25:02 -0300

    Se agregan cambios con un cambio al tipo de variable Observable

--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -4,7 +4,7 @@ export const CLIENTES: Cliente[] = [
     {id: 1 ,nombre:'Peter',apellido:'Parcker',email:'araña@mail.com',createAt:'2019-11-11'},
     {id: 2 ,nombre:'Tony',apellido:'Stark',email:'iroman@mail.com',createAt:'2019-12-12'},
     {id: 3 ,nombre:'Bruce',apellido:'Banner',email:'hulk@mail.com',createAt:'2017-01-01'},
-    {id: 4 ,nombre:'Natacha',apellido:'Romanov',email:'viuda@mail.com',createAt:'2019-05-09'},
+    {id: 4 ,nombre:'Natachita',apellido:'Romanov',email:'viuda@mail.com',createAt:'2019-05-09'},
     {id: 5 ,nombre:'Steve',apellido:'Rogers',email:'capi@mail.com',createAt:'1999-10-11'},
     {id: 7 ,nombre:'Wanda',apellido:'Encantada',email:'bruja@mail.com',createAt:'2016-09-03'},
     {id: 8 ,nombre:'Leiya',apellido:'Skywaker',email:'princesa@mail.comn',createAt:'1980-01-11'},
