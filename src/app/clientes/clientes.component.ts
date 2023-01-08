3ea3f91d95ab509d559a5e1ae354f6914e963237
unknown <coke.jgomez@gmail.com>
2023-01-08 19:02:13 -0300

    Se desagrega la data en un json apartegit status

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import {CLIENTES} from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,22 +8,11 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit{
 
-  clientes: Cliente[] = [
-    {id: 1 ,nombre:'Peter',apellido:'Parcker',email:'araña@mail.com',createAt:'2019-11-11'},
-    {id: 2 ,nombre:'Tony',apellido:'Stark',email:'iroman@mail.com',createAt:'2019-12-12'},
-    {id: 3 ,nombre:'Bruce',apellido:'Banner',email:'hulk@mail.com',createAt:'2017-01-01'},
-    {id: 4 ,nombre:'Natacha',apellido:'Romanov',email:'viuda@mail.com',createAt:'2019-05-09'},
-    {id: 5 ,nombre:'Steve',apellido:'Rogers',email:'capi@mail.com',createAt:'1999-10-11'},
-    {id: 7 ,nombre:'Wanda',apellido:'Encantada',email:'bruja@mail.com',createAt:'2016-09-03'},
-    {id: 8 ,nombre:'Leiya',apellido:'Skywaker',email:'princesa@mail.comn',createAt:'1980-01-11'},
-    {id: 9 ,nombre:'Luke',apellido:'Skywaker',email:'jedi@mail.com',createAt:'1982-05-07'},
-    {id: 10 ,nombre:'Dark',apellido:'Vader',email:'soytupadre@mail.com',createAt:'1970-08-10'},
-    {id: 11 ,nombre:'Han',apellido:'Solo',email:'falcon@mail.com',createAt:'1992-02-03'}
-  ]  ;
+  clientes: Cliente[] = [];
 
   constructor(){}
 
   ngOnInit(): void {
-    
+    this.clientes = CLIENTES;
   }
 }
