9bfafaf914be4e049e07ef03ec01474ef86a7e52
unknown <coke.jgomez@gmail.com>
2023-01-14 15:12:08 -0300

    Se agrega el formulario en el front

---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Cliente } from './cliente';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent {
+
+  cliente: Cliente = new Cliente();
+  titulo: string = "Crear Cliente"
+  constructor(){ }
+
+  ngOnInit(): void {
+    
+  }
+
+  public create():void{
+    console.log("Clicked")
+    console.log(this.cliente)
+  }
+}
