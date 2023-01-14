b14dbc9e7c1e2737e9dba5ba5d12eea18288e451
unknown <coke.jgomez@gmail.com>
2023-01-13 23:15:11 -0300

    Se agrega la url del endpoint en el servicio de cliente

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json';
 import { Cliente } from './cliente';
-import { Observable,of } from 'rxjs';
+import { Observable,of,map } from 'rxjs';
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:9090/api/clientes';
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
-    return of(CLIENTES);
+   //return estatico
+    // return of(CLIENTES);
+   //una forma es hacer un cast de cliente a la urlEndPoint
+   //return this.http.get<Cliente[]>(this.urlEndPoint);
+    //otra forma es con MAP
+    return this.http.get(this.urlEndPoint).pipe(
+      map( response => response as Cliente[])
+    );
   }
 }
